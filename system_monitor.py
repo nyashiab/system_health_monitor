1353f70d08189b66a12a99b7f3f39c0b790db70a
nyashiab <nyboxx06@gmail.com>
2025-12-22 20:02:42 -0500

    slack alert update

--- system_monitor.py
+++ system_monitor.py
@@ -2,6 +2,7 @@
 import psutil 
 import json
 from datetime import datetime
+import requests
 
 def get_system_metrics():
     return{
@@ -15,11 +16,16 @@ def log_metrics(metrics, filename = "system_metrics.log"):
     with open(filename, 'a') as f:
         f.write(json.dumps(metrics) + "\n")
 
+def send_slack_alert(message):
+    webhook_url = "https://hooks.slack.com/services/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"
+    payload = {"text": message}
+    requests.post(webhook_url, json=payload)
+
 if __name__ == "__main__":
     metrics = get_system_metrics()
     print(f"Current Metrics: {metrics}")
     log_metrics(metrics)
 
-    # Alert if CPU > 90%
+    # Alert message sent to Slack if CPU > 90%
     if metrics["cpu_usage"] > 90:
-        print("ALERT: High CPU usage!")
+        send_slack_alert(f"🚨🚨 HIGH CPU : {metrics['cpu_usage']}%")
